301aa76988c2315efaf47852cea3604a9f21783c
arjun-natarajan99 <arjun.nat18@gmail.com>
2021-12-16 22:11:36 -0800

    Deleted ios Pods

--- ios/Pods/Target Support Files/Pods-mobiletodolist-mobiletodolistTests/Pods-mobiletodolist-mobiletodolistTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_mobiletodolist_mobiletodolistTests : NSObject
-@end
-@implementation PodsDummy_Pods_mobiletodolist_mobiletodolistTests
-@end
--- ios/Pods/Target Support Files/Pods-mobiletodolist/Pods-mobiletodolist-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_mobiletodolist : NSObject
-@end
-@implementation PodsDummy_Pods_mobiletodolist
-@end
